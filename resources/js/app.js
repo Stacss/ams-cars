784de89b90c2adbf236ad3ea0a2b0a73d0059244
postnikov-stas <postnikov.sa@ya.ru>
2024-06-06 20:16:10 +0300

    установка vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import CarSelector from './components/CarSelector.vue';
+
+const app = createApp({});
+app.component('car-selector', CarSelector);
+app.mount('#app');
